81cbcc9fe3233c6cb775f0846c7ef6bf4c26b647
JS3322 <js@cleancode.kr>
2022-08-18 17:37:44 +0900

    init2

--- output_excel/__init.py
+++ output_excel/__init.py
@@ -1,9 +1,19 @@
 # import numpy as np
 import numpy as np
+import pandas as pd
 
+area = ['번호', '이름', '1-1-2', '1-3-0', '2-1-3']
+input = [1, '김준석', 5, 4, 3]
 
-list1 = [1,2,3,4]
-a = np.array(list1)
-print(a.shape)
+df = pd.DataFrame(area, row=['area'])
+df['input'] = input
 
-print("hello world")
+df.to_csv("source/output.csv", index = False)
+
+print(df)
+
+# list1 = [1,2,3,4]
+# a = np.array(list1)
+# print(a.shape)
+#
+# print("hello world")
